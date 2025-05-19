879d53bbd9557791d4f428dc98aeb9a9c77ebfb7
Shuanzi <1373833569@qq.com>
2025-05-19 20:46:40 +0800

    E_DES&D_DES.py

--- DES.py
+++ DES.py
@@ -225,8 +225,70 @@ def E_DES(m, key):
         cipher+=str(c)
     return hex(int(cipher,2))
 
+def D_DES(c, key):
+    key0 = pc1(key)
+    round_key = left_loop_shift_and_pc2(key0)
+    for i in range(16):
+        round_key[i],round_key[15-i]=round_key[15-i],round_key[i]
+    # 处理64bits明文（与密钥处理方式相同）
+    # 前提：明文要么为十六进制要么直接为二进制）
+    # 如果是十六进制就先变为二进制再变成列表
+    if c[0:2] == "0x":
+        c0 = hex2bin2list(c)
+    else:
+        c0 = [int(i) for i in c]
+    # 初始置换IP表，结果明文总长64bits没变，只是打乱顺序
+    ipTable = [
+        58, 50, 42, 34, 26, 18, 10, 2,
+        60, 52, 44, 36, 28, 20, 12, 4,
+        62, 54, 46, 38, 30, 22, 14, 6,
+        64, 56, 48, 40, 32, 24, 16, 8,
+        57, 49, 41, 33, 25, 17, 9, 1,
+        59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5,
+        63, 55, 47, 39, 31, 23, 15, 7]
+    c1 = []
+    for i in range(64):
+        c1.append(c0[ipTable[i] - 1])
+
+    # m1分成左右两部分L[0],R[0]
+    L = [[] for i in range(17)]
+    R = [[] for i in range(17)]
+    L[0], R[0] = c1[:32], c1[32:]
+
+    # 调用F(R[i-1],round_key[i-1])函数
+    for i in range(1,17):
+        temp = F(R[i-1],round_key[i-1])
+        temp=[XOR(temp[j],L[i-1][j]) for j in range(32)]
+        L[i]=R[i-1]
+        R[i]=temp
+
+    # 将L[16]与R[16]拼接再进行逆初始置换得到密文
+    c = R[16]+L[16]
+    # 初始逆置换IP^-1表
+    ipReverseTable = [
+        40, 8, 48, 16, 56, 24, 64, 32,
+        39, 7, 47, 15, 55, 23, 63, 31,
+        38, 6, 46, 14, 54, 22, 62, 30,
+        37, 5, 45, 13, 53, 21, 61, 29,
+        36, 4, 44, 12, 52, 20, 60, 28,
+        35, 3, 43, 11, 51, 19, 59, 27,
+        34, 2, 42, 10, 50, 18, 58, 26,
+        33, 1, 41, 9, 49, 17, 57, 25]
+
+    result = []
+    for i in range(64):
+        result.append(c[ipReverseTable[i] - 1])
+
+    cipher=''
+    for c in result:
+        cipher+=str(c)
+    return hex(int(cipher,2))
+
 if __name__=="__main__":
     m='0x3031323334353637'
     key='0x3132333435363738'
     c=E_DES(m,key)
     print(c)
+    m0=D_DES(c,key)
+    print(m)
